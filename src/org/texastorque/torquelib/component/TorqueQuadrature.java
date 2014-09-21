c8bd5d70c6a64b81bb95043160136cd4382616e9
Gijs Landwehr <github@fam-landwehr.nl>
2014-09-20 22:43:32 -0500

    Added encoding type constants to encoder

--- src/org/texastorque/torquelib/component/TorqueQuadrature.java
+++ src/org/texastorque/torquelib/component/TorqueQuadrature.java
@@ -6,6 +6,9 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class TorqueQuadrature extends TorqueEncoder
 {
+    public static final CounterBase.EncodingType k1X = CounterBase.EncodingType.k1X;
+    public static final CounterBase.EncodingType k2X = CounterBase.EncodingType.k2X;
+    public static final CounterBase.EncodingType k4X = CounterBase.EncodingType.k4X;
     
     public TorqueQuadrature(int aChannel, int bChannel, boolean reverseDirection)
     {
