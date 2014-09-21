e947c342cdb10bf41b4ccbe250f5b3d25c0aba21
Gijs Landwehr <github@fam-landwehr.nl>
2014-09-20 22:45:15 -0500

    Encoding constants for counter

--- src/org/texastorque/torquelib/component/TorqueCounter.java
+++ src/org/texastorque/torquelib/component/TorqueCounter.java
@@ -6,34 +6,33 @@ import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.DigitalSource;
 
-public class TorqueCounter extends TorqueEncoder
-{
+public class TorqueCounter extends TorqueEncoder {
+
+    public static final CounterBase.EncodingType k1X = CounterBase.EncodingType.k1X;
+    public static final CounterBase.EncodingType k2X = CounterBase.EncodingType.k2X;
+
     private MovingAverageFilter filter;
-    
-    public TorqueCounter(int port)
-    {
+
+    public TorqueCounter(int port) {
         encoder = new Counter(port);
-        
+
         filter = new MovingAverageFilter(1);
     }
-    
-    public TorqueCounter(CounterBase.EncodingType encodingType, DigitalSource upSource, DigitalSource downSource, boolean reverse)
-    {
+
+    public TorqueCounter(CounterBase.EncodingType encodingType, DigitalSource upSource, DigitalSource downSource, boolean reverse) {
         encoder = new Counter(encodingType, upSource, downSource, reverse);
-        
+
         filter = new MovingAverageFilter(1);
         filter.reset();
     }
-    
-    public void setFilterSize(int size)
-    {
+
+    public void setFilterSize(int size) {
         filter = new MovingAverageFilter(size);
         filter.reset();
     }
-    
+
     @Override
-    public void calc()
-    {
+    public void calc() {
         double currentRate = 1.0 / encoder.getPeriod();
         filter.setInput(currentRate);
         filter.run();
@@ -41,10 +40,9 @@ public class TorqueCounter extends TorqueEncoder
         currentPosition = encoder.get();
         rate = filter.getAverage();
     }
-    
+
     @Override
-    public void reset()
-    {
+    public void reset() {
         filter.reset();
         encoder.reset();
     }
