c11f8530eb9841d61c9e56133380118ddc156250
Matthew Webb <matthewwebb65@yahoo.com>
2014-12-04 15:49:17 -0800

    Remove average filter from counter

--- src/org/texastorque/torquelib/component/TorqueCounter.java
+++ src/org/texastorque/torquelib/component/TorqueCounter.java
@@ -12,32 +12,16 @@ public class TorqueCounter extends TorqueEncoder {
     public static final CounterBase.EncodingType k1X = CounterBase.EncodingType.k1X;
     public static final CounterBase.EncodingType k2X = CounterBase.EncodingType.k2X;
 
-    private MovingAverageFilter filter;
-
     public TorqueCounter(int port) {
         encoder = new Counter(new DigitalInput(port));
-
-        filter = new MovingAverageFilter(1);
-        filter.reset();
     }
 
     public TorqueCounter(int countPort, int directionPort, boolean reverse, CounterBase.EncodingType encodingype) {
         encoder = new Counter(encodingype, new DigitalInput(countPort), new DigitalInput(directionPort), reverse);
-
-        filter = new MovingAverageFilter(1);
-        filter.reset();
     }
 
     public TorqueCounter(AnalogTrigger trigger) {
         encoder = new Counter(trigger);
-
-        filter = new MovingAverageFilter(1);
-        filter.reset();
-    }
-
-    public void setFilterSize(int size) {
-        filter = new MovingAverageFilter(size);
-        filter.reset();
     }
 
     public void calc() {
@@ -48,16 +32,12 @@ public class TorqueCounter extends TorqueEncoder {
         instantRate = 1.0 / encoder.getPeriod();
         acceleration = (secantRate - previousRate) / (currentTime - previousTime);
         
-        filter.setInput(secantRate);
-        filter.run();
-
         previousTime = currentTime;
         previousPosition = currentPosition;
         previousRate = secantRate;
     }
 
     public void reset() {
-        filter.reset();
         encoder.reset();
     }
 }
