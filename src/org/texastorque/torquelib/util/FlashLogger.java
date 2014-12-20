7a35f3ab40910298bd59e95bbb00c9c293991b6d
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-20 13:51:25 -0600

    Changed logging to make a new file every run

--- src/org/texastorque/torquelib/util/FlashLogger.java
+++ src/org/texastorque/torquelib/util/FlashLogger.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class FlashLogger {
 
     private FileWriter writer;
-    
+
     private final String filePath = "/media/sda1/logging/";
     private String fileName;
+
+    private boolean enabled;
 
     /**
      * Create a new flash drive logger.
@@ -24,14 +24,13 @@ public class FlashLogger {
     public FlashLogger() {
         reset();
     }
-    
-    public void reset()
-    {
+
+    public void reset() {
         Date currentDate = new Date();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM--dd::HH--mm", Locale.US);
         String time = format.format(currentDate);
         fileName = "log" + time;
-        
+
         File file = new File(filePath + fileName);
         try {
             file.createNewFile();
@@ -39,23 +38,36 @@ public class FlashLogger {
         } catch (IOException e) {
             System.err.println("IOException in creating flash logger.");
         }
+
+        enabled = false;
+    }
+
+    public void enable() {
+        enabled = true;
+    }
+
+    public void disable() {
+        enabled = false;
     }
 
     public void log(String msg) {
-        try {
-            writer.write(msg);
-        } catch (IOException e) {
-            System.err.println("IOException in writing to flash logger.");
-        }
-        
-        try {
-            writer.flush();
-        } catch (IOException ex) {
-            System.err.println("IOException in flushing.");
+        if (enabled) {
+            try {
+                writer.write(msg);
+            } catch (IOException e) {
+                System.err.println("IOException in writing to flash logger.");
+            }
+
+            try {
+                writer.flush();
+            } catch (IOException ex) {
+                System.err.println("IOException in flushing.");
+            }
         }
     }
 
     public void close() {
+        enabled = false;
         try {
             writer.close();
         } catch (IOException e) {
