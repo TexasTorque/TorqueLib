2b3d64d6a9443f41e4dbac2b93d225d04975077e
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-20 13:51:10 -0600

    Changed logging to make a new file every run

--- src/org/texastorque/torquelib/util/FlashLogger.java
+++ src/org/texastorque/torquelib/util/FlashLogger.java
@@ -3,39 +3,39 @@ package org.texastorque.torquelib.util;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FlashLogger {
 
     private FileWriter writer;
-
-    /**
-     * Create a new flash drive logger with detected flash drive. Work in
-     * progress.
-     */
-    public FlashLogger() {
-        for (File dir : File.listRoots()) {
-            for (File f : dir.listFiles()) {
-                if (f.getName().equals("flashlog.txt")) {
-                    try {
-                        writer = new FileWriter(f);
-                    } catch (IOException e) {
-                        System.err.println("IOException in creating flash logger.");
-                    }
-                }
-            }
-        }
-    }
+    
+    private final String filePath = "/media/sda1/logging/";
+    private String fileName;
 
     /**
      * Create a new flash drive logger.
      *
      * @param filePath File path of logging file in flash drive.
      */
-    public FlashLogger(String filePath) {
-        File file = new File(filePath);
+    public FlashLogger() {
+        reset();
+    }
+    
+    public void reset()
+    {
+        Date currentDate = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM--dd::HH--mm", Locale.US);
+        String time = format.format(currentDate);
+        fileName = "log" + time;
+        
+        File file = new File(filePath + fileName);
         try {
             file.createNewFile();
-            writer = new FileWriter(file);
+            writer = new FileWriter(file, true);
         } catch (IOException e) {
             System.err.println("IOException in creating flash logger.");
         }
@@ -47,13 +47,19 @@ public class FlashLogger {
         } catch (IOException e) {
             System.err.println("IOException in writing to flash logger.");
         }
+        
+        try {
+            writer.flush();
+        } catch (IOException ex) {
+            System.err.println("IOException in flushing.");
+        }
     }
 
     public void close() {
         try {
             writer.close();
         } catch (IOException e) {
-            System.err.println("Error in closing flash logger.");
+            System.err.println("IOException in closing flash logger.");
         }
     }
 }
