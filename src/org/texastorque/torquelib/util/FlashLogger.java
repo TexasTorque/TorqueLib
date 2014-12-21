73cbdc64e11e2aea34438cab580ba864a52f3df1
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-21 12:10:03 -0600

    Finished logging system

--- src/org/texastorque/torquelib/util/FlashLogger.java
+++ src/org/texastorque/torquelib/util/FlashLogger.java
@@ -1,62 +1,85 @@
 package org.texastorque.torquelib.util;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
-public class FlashLogger {
+public final class FlashLogger {
 
-    private FileWriter writer;
+    private BufferedWriter writer;
 
-    private final String filePath = "C:\\users\\Gijs\\Desktop\\";
+    private final String filePath = "/media/sda1/logging/";
     private String fileName;
 
     private boolean enabled;
+    private boolean firstWrite;
+    
+    private ArrayList<Loggable> loggedSystems;
 
     /**
      * Create a new flash drive logger.
      *
-     * @param filePath File path of logging file in flash drive.
      */
     public FlashLogger() {
+        loggedSystems = new ArrayList<>();
         reset();
     }
 
+    /**
+     * Make a new file and reset the logging system.
+     * 
+     */
     public void reset() {
         Date currentDate = new Date();
         SimpleDateFormat format = new SimpleDateFormat("MM-dd-yy-HH-mm", Locale.US);
         String time = format.format(currentDate);
-        fileName = "LOG " + time + ".txt";
+        fileName = "LOG " + time + ".csv";
 
         try {
             File file = new File(filePath + fileName);
             file.createNewFile();
-            writer = new FileWriter(file);
-        } catch (Exception e) {
+            writer = new BufferedWriter(new FileWriter(file));
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             System.err.println("IOException in creating flash logger.");
         }
 
         enabled = false;
+        firstWrite = true;
     }
 
-    public void enable() {
-        enabled = true;
-    }
-
-    public void disable() {
-        enabled = false;
-    }
-
-    public void log(String msg) {
+    /**
+     * Log data from the systems registered for logging.
+     * 
+     */
+    public void log() {
         if (enabled) {
             try {
-                writer.write(msg + "\n");
+                if (firstWrite)
+                {
+                    String names = "";
+                    for (Loggable l : loggedSystems)
+                    {
+                        names += l.getLogNames();
+                    }
+                    writer.write(names + "\n");
+                    firstWrite = false;
+                } else {
+                    String vals = "";
+                    for (Loggable l : loggedSystems)
+                    {
+                        vals += l.getLogValues();
+                    }
+                    writer.write(vals + "\n");
+                }
+                
             } catch (IOException e) {
-                System.err.println("IOException in writing to flash logger.");
+                System.err.println("IOException in writing to file.");
             }
 
             try {
@@ -67,11 +90,34 @@ public class FlashLogger {
         }
     }
 
+    /**
+     * Close the file after a logging session is completed.
+     * 
+     */
     public void close() {
         enabled = false;
         try {
             writer.close();
-        } catch (Exception e) {
+        } catch (IOException e) {
+            System.err.println("IOException in closing filewriter.");
         }
+    }
+    
+    /**
+     * Register a new system for logging.
+     * 
+     * @param log The new system to be logged.
+     */
+    public void addLoggable(Loggable log)
+    {
+        loggedSystems.add(log);
+    }
+    
+    public void enable() {
+        enabled = true;
+    }
+
+    public void disable() {
+        enabled = false;
     }
 }
