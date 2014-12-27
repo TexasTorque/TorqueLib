02eaf12083b3a549934c94dc4156d8b11915797e
Gijs Landwehr <gijs@fam-landwehr.nl>
2014-12-27 13:20:32 -0600

    Removed unnecesary error handling from pyconsole

--- python/pyConsole.py
+++ python/pyConsole.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-# pyConsole
+#
+#  --- pyConsole ---
+#
+# Author: Texas Torque
+#
 # Simple python script to read and display console output from your roboRio. This is a
 # standalone alternative to RioLog that allows you to use another IDE without needing eclipse
 # to see console output.
@@ -10,35 +14,23 @@ import time
 
 PORT = 6666
 BUFFER_SIZE = 1024
-RETRY_PERIOD = 1.0
 
 def main():
-    connected = False
-    last_error_time = time.time()
-    while 1:
-        current_time = time.time()
-        if not connected and current_time - RETRY_PERIOD > last_error_time:
-            try:
-                print("Creating socket.")
-                rioSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                rioSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                #rioSocket.settimeout(0.1)
-                rioSocket.bind(("", PORT))
-                print("Socket created.")
-                connected = True
-            except:
-                if current_time - RETRY_PERIOD > last_error_time:
-                    print("Could not create socket")
-                    last_time = time.time()
-
         try:
-            lines, addr = rioSocket.recvfrom(BUFFER_SIZE)
-            print(lines[:len(lines)-1])
+            print("Creating socket.")
+            rioSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            rioSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            rioSocket.bind(("", PORT))
+            print("Socket created.")
+
+            print("Robot output:\n\n")
+            while 1:
+                lines, addr = rioSocket.recvfrom(BUFFER_SIZE)
+                print(lines[:len(lines)-1])
+            
         except:
-            connected = False
-            if current_time - RETRY_PERIOD > last_error_time:
-                print("Could not retrieve data")
-                last_time = time.time()
+            print("Could not create socket")
+            last_error_time = time.time()
 
 if __name__ == "__main__":
     main()
