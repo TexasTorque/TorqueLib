d41fad968838237971047e562cb968a8133eee4b
Gijs Landwehr <gijs@fam-landwehr.nl>
2015-01-13 07:59:13 -0600

    Add isDone and onTarget to PV

--- src/org/texastorque/torquelib/controlLoop/TorquePV.java
+++ src/org/texastorque/torquelib/controlLoop/TorquePV.java
@@ -1,51 +1,72 @@
 package org.texastorque.torquelib.controlLoop;
 
 public class TorquePV extends ControlLoop {
-    
+
     private double kP;
     private double kV;
     private double kFFV;
     private double kFFA;
- 
+
+    private TorqueTMP profile;
+    private double actualPosition;
+    private double actualVelocity;
+    private double positionDoneRange;
+
     public TorquePV() {
         super();
-        
+
         kP = 0.0;
         kV = 0.0;
         kFFV = 0.0;
         kFFA = 0.0;
     }
-    
+
     public double calculate(TorqueTMP tmProfile, double currentPosition, double currentVelocity) {
-        setPoint = tmProfile.getCurrentVelocity();
+        profile = tmProfile;
+        setPoint = profile.getCurrentVelocity();
         currentValue = currentVelocity;
-        
+
         double output = 0.0;
-        
+
         //Position P
-        double error  = tmProfile.getCurrentPosition() - currentPosition;
+        double error = profile.getCurrentPosition() - currentPosition;
         output += (error * kP);
 
         //Velocity P
-        double velocityError = tmProfile.getCurrentVelocity() - currentVelocity;
+        double velocityError = profile.getCurrentVelocity() - currentVelocity;
         output += (velocityError * kV);
-        
+
         //Velocity FeedForward
-        output += (tmProfile.getCurrentVelocity() * kFFV);
-        
+        output += (profile.getCurrentVelocity() * kFFV);
+
         //Acceleration FeedForward
-        output += (tmProfile.getCurrentAcceleration() * kFFA);
-        
+        output += (profile.getCurrentAcceleration() * kFFA);
+
         return output;
     }
-    
+
     public void setGains(double p, double v, double ffV, double ffA) {
         kP = p;
         kV = v;
         kFFV = ffV;
         kFFA = ffA;
     }
-    
+
     public void reset() {
+    }
+
+    public boolean isDone() {
+        if ((Math.abs(profile.getCurrentPosition() - actualPosition) < positionDoneRange)
+                && Math.abs(profile.getCurrentVelocity() - actualVelocity) < doneRange) {
+            doneCyclesCount++;
+        } else {
+            doneCyclesCount = 0;
+        }
+        
+        return (doneCyclesCount > minDoneCycles);
+    }
+    
+    public boolean onTrack() {
+        return Math.abs(profile.getCurrentVelocity() - actualVelocity) < doneRange;
     }
 }
