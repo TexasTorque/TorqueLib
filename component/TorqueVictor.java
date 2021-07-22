85b1c878da6b9897c44289de8f157c07f7e30a64
realSaddy <jrp30074@gmail.com>
2021-07-22 17:59:51 -0500

    Make victor have follower capability

--- component/TorqueVictor.java
+++ component/TorqueVictor.java
@@ -1,14 +1,23 @@
 package org.texastorque.torquelib.component;
 
+import java.util.ArrayList;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class TorqueVictor extends VictorSPX {
-    public TorqueVictor(int port){
+    private ArrayList<VictorSPX> followers = new ArrayList<VictorSPX>();
+
+    public TorqueVictor(int port) {
         super(port);
     }
-    
+
     public void set(double val) {
         set(ControlMode.PercentOutput, val);
+        followers.forEach(v -> v.set(ControlMode.PercentOutput, val));
+    }
+
+    public void addFollower(int port) {
+        followers.add(new VictorSPX(port));
     }
 }
