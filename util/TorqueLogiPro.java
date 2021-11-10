3626072155e73dd5d061af8695ef582b12714c3c
Juicestus <jus@gtsbr.org>
2021-11-10 09:56:22 -0600

    Buttons on throttle maybe work?

--- util/TorqueLogiPro.java
+++ util/TorqueLogiPro.java
@@ -6,31 +6,31 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class TorqueLogiPro extends Joystick {
     private final int port;
     private double deadband;
-    private static final int NUM_BUTTONS = 12;
     private static final double DEADBAND_DEF = .01;
     public JoystickButton[] buttons;
 
     public TorqueLogiPro(int port) {
         super(port);
         this.port = port;
-        buttons = new JoystickButton[NUM_BUTTONS+1];
-		for (int i = 1; i < NUM_BUTTONS+1; i++)
-			buttons[i] = new JoystickButton(this, i);
         deadband = DEADBAND_DEF;
     }
 
     public TorqueLogiPro(int port, double deadband) {
         super(port);
         this.port = port;
-        buttons = new JoystickButton[NUM_BUTTONS+1];
-		for (int i = 1; i < NUM_BUTTONS+1; i++)
-			buttons[i] = new JoystickButton(this, i);
         this.deadband = deadband;
     }
 
     private double deadbanded(double value) {
         return (value < deadband && value > -deadband) ? 0 : value;
     }
+
+    public boolean getButtonByIndex(int index) {
+        return getRawButton(index);
+    }
+
+    public boolean getTrigger() { return getRawButton(0); }
+    public boolean getThumb() { return getRawButton(1); }
 
     public double getRoll() { return deadbanded(getX()); } 
     public double getPitch() { return deadbanded(-getY()); }
