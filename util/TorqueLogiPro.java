3fc7ac53d57c87b5bcfaf2870d6d2821d84a9ce8
Juicestus <jus@gtsbr.org>
2021-11-11 10:40:14 -0600

    Enum for POV + throttle

--- util/TorqueLogiPro.java
+++ util/TorqueLogiPro.java
@@ -3,6 +3,9 @@ package org.texastorque.torquelib.util;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
+/**
+ * @author TexasTorque 2021
+ */
 public class TorqueLogiPro extends Joystick {
     protected final int port;
     private double deadband;
@@ -28,10 +31,46 @@ public class TorqueLogiPro extends Joystick {
         return getRawButton(index);
     }
 
-    public boolean getTrigger() { return getRawButton(0); }
-    public boolean getThumb() { return getRawButton(1); }
+    public boolean getTrigger() { 
+        return getRawButton(0); 
+    }
 
-    public double getRoll() { return deadbanded(getX()); } 
-    public double getPitch() { return deadbanded(-getY()); }
-    public double getYaw() { return deadbanded(getZ()); }
+    public boolean getThumb() { 
+        return getRawButton(1); 
+    }
+
+    public PovState getPovState() { 
+        return PovState.NORTH; // Fix this
+    }
+
+    public double getRoll() { 
+        return deadbanded(getX()); // getRawAxis(1)
+    }
+
+    public double getPitch() { 
+        return deadbanded(-getY()); // getRawAxis(2)
+    }
+
+    public double getYaw() { 
+        return deadbanded(getZ()); // getRawAxis(3)
+    }
+
+    public double getThrottle() { 
+        return deadbanded(getRawAxis(4)); 
+    }
+
+    public static enum PovState {
+        NORTH("NORTH"), NORTH_EAST("NORTH EAST"), EAST("EAST"), SOUTH_EAST("SOUTH EAST"), 
+        SOUTH("SOUTH"), SOUTH_WEST("SOUTH WEST"), WEST("WEST"), NORTH_WEST("SOUTH WEST"); 
+
+        private String value;
+
+        PovState(String value) {
+            this.value = value;
+        }
+
+        String asString() { 
+            return value; 
+        }
+    }
 }
