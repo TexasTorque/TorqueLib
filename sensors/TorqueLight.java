89290604b3a833f5fc25821596e6893e661915a4
Juicestus <jus@gtsbr.org>
2022-05-09 22:42:58 -0500

    shooter shud be done

--- sensors/TorqueLight.java
+++ sensors/TorqueLight.java
@@ -1,18 +1,65 @@
 package org.texastorque.torquelib.sensors;
 
-import org.texastorque.torquelib.sensors.base.TorqueVisionCamera;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-// import org.photonvision.PhotonCamera;
-// import org.photonvision.PhotonUtils;
-// import org.photonvision.targeting.PhotonPipelineResult;
-// import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
-public class TorqueLight implements TorqueVisionCamera {
-    // private final PhotonCamera torqueCam = new PhotonCamera(NetworkTableInstance.getDefault(),
-    // "torquecam");
+public class TorqueLight {
+    private final PhotonCamera cam;
 
-    @Override
-    public double getDistance() {
-        return 0;
+    private PhotonPipelineResult result;
+    private PhotonTrackedTarget target;
+
+    private final double cameraHeight, targetHeight;
+    private final Rotation2d angle;
+    
+    public TorqueLight(final double cameraHeight, final double targetHeight, final Rotation2d angle) { 
+        this.cam = new PhotonCamera(NetworkTableInstance.getDefault(),"torquecam");
+        this.result = new PhotonPipelineResult();
+        this.target = new PhotonTrackedTarget();
+
+        this.cameraHeight = cameraHeight;
+        this.targetHeight = targetHeight;
+        this.angle = angle;
     }
+
+    public void update() {
+        result = cam.getLatestResult();
+        if (result.hasTargets())
+            target = result.getBestTarget();
+    }
+
+    public boolean hasTargets() {
+        return result.hasTargets();
+    }
+
+    public double getTargetArea() {
+        return target.getArea();
+    }
+
+    public double getTargetYaw() {
+        return target.getYaw();
+    }
+
+    public double getTargetPitch() {
+        return target.getPitch();
+    }
+
+    public Transform2d getCameraToTarget() {
+        return target.getCameraToTarget();
+    }
+
+    public double getDistance() {
+        return PhotonUtils.calculateDistanceToTargetMeters(
+                cameraHeight, targetHeight, angle.getRadians(), 
+                Units.degreesToRadians(getTargetPitch())
+        );
+    }
+   
 }
--- sensors/base/TorqueVisionCamera.java
+++
@@ -1,8 +0,0 @@
-package org.texastorque.torquelib.sensors.base;
-
-/**
- * An interface for vision cameras.
- */
-public interface TorqueVisionCamera {
-    public double getDistance();
-}
