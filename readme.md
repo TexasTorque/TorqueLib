2e46464bfdb4139ef66f0bb887561414ba79704b
Juicestus <jus@gtsbr.org>
2022-05-11 10:40:38 -0500

    torquelight docs + authors

--- readme.md
+++ readme.md
@@ -1,30 +1,15 @@
 # TorqueLib
 
-Code library created by and used within FRC 1477 Texas Torque.
+Code library created by and used within FRC 1477, Texas Torque.
 
 ## 2023 Refactor
 
 This library, along with the Texas Torque project structure,
 is being overhauled for the 2023 season. These are breaking changes.
 
-## Q/A
+## Authors
 
-### How do I get this in my project?
-
-`git submodule add https://github.com/TexasTorque/TorqueLib torquelib`
-
-### How do I clone repositories with this in it?
-
-`git clone --recursive origin branch`
-
-### How do I pull repositories with this in it?
-
-`git submodule update --init --recursive`
-
-### How do I update this when its in another repository?
-
-`git submodule update --remote --merge`
-
-### Other question?
-
-Try looking it up or you can ask Jack
+- Gijs Landwehr (2013-2014)
+- Matthew Webb (2013-2014)
+- Jack Pittenger (2020-2023)
+- Justus Languell (2021-2024)
--- sensors/TorqueLight.java
+++ sensors/TorqueLight.java
@@ -9,6 +9,15 @@ import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+/**
+ * Java client interface for the TorqueLight.
+ * 
+ * TorqueLight is a vision camera developed by Texas Torque
+ * that runs PhotonVision.
+ * 
+ * @author Jack Pittenger 
+ * @author Justus Languell
+ */
 public class TorqueLight {
     private final PhotonCamera cam;
 
@@ -18,9 +27,15 @@ public class TorqueLight {
     private final double cameraHeight, targetHeight;
     private final Rotation2d angle;
 
+    /**
+     * Creates a new TorqueLight object with desired physical parameters.
+     * 
+     * @param cameraHeight The height of the camera in meters.
+     * @param targetHeight The height of the target in meters.
+     * @param angle        The angle of the camera as a Rotation2d.
+     */
     public TorqueLight(final double cameraHeight, final double targetHeight, final Rotation2d angle) {
-        // this.cam = new PhotonCamera(NetworkTableInstance.getDefault(),"torquecam");
-        this.cam = null;
+        this.cam = new PhotonCamera(NetworkTableInstance.getDefault(),"torquecam");
         this.result = new PhotonPipelineResult();
         this.target = new PhotonTrackedTarget();
 
@@ -29,21 +44,57 @@ public class TorqueLight {
         this.angle = angle;
     }
 
+    /**
+     * Call this periodically to update the vision state.
+     */
     public final void update() {
         result = cam.getLatestResult();
-        if (result.hasTargets()) target = result.getBestTarget();
+        if (result.hasTargets()) 
+            target = result.getBestTarget();
     }
 
+    /**
+     * Does the camera have any targets.
+     * 
+     * @return Do we have targets or not.
+     */
     public final boolean hasTargets() { return result.hasTargets(); }
 
+    /**
+     * Returns the area of the target.
+     * 
+     * @return the area of the target.
+     */
     public final double getTargetArea() { return target.getArea(); }
 
+    /**
+     * Returns the yaw angle of the target as degrees.
+     * 
+     * @return The yaw angle of the target as degrees.
+     */
     public final double getTargetYaw() { return target.getYaw(); }
 
+     /**
+     * Returns the pitch angle of the target as degrees.
+     * 
+     * @return The pitch angle of the target as degrees.
+     */
     public final double getTargetPitch() { return target.getPitch(); }
 
+    /**
+     * Returns a transformation that represents the distance 
+     * betweeen the camera and the target.
+     * 
+     * @return Transformation2d object that represents the 
+     *         distance betweeen the camera and the target.
+     */
     public final Transform2d getCameraToTarget() { return target.getCameraToTarget(); }
 
+    /**
+     * Calculates the distance to the target in meters.
+     * 
+     * @return The distance to the target in meters.
+     */
     public final double getDistance() {
         return PhotonUtils.calculateDistanceToTargetMeters(cameraHeight, targetHeight, angle.getRadians(),
                                                            Units.degreesToRadians(getTargetPitch()));
