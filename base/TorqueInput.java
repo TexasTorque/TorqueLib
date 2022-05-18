90d1a2c8ef26d0279121291eecd6da1d835e4850
Juicestus <jus@gtsbr.org>
2022-05-17 21:20:25 -0500

    TorqueInput changes

--- base/TorqueInput.java
+++ base/TorqueInput.java
@@ -1,33 +1,16 @@
 package org.texastorque.torquelib.base;
 
+import org.texastorque.torquelib.util.GenericController;
+
 /**
- * @apiNote This is a big bruh moment...
- *          You overload the update method
- *          You run it using the run method
- *          (this is for the assist sequence)
- *
- * @author Justus (see me for questions)
- *
- * @deprecated No longer used in new project structure.
+ * Input base class. Holds references to controllers.
+ * 
+ * @author Justus Languell
  */
-@Deprecated
 public abstract class TorqueInput {
-    private boolean blocked = false;
-
-    public final void block() { blocked = true; }
-
-    public final void unblock() { blocked = false; }
-
-    public final boolean isBlocked() { return blocked; }
+    protected GenericController driver, operator;
 
     public abstract void update();
 
-    protected void reset(){};
-
-    public void smartDashboard(){};
-
-    public final void run() {
-        if (!blocked) update();
-        unblock();
-    }
+    public void smartDashboard() {}
 }
---
+++ base/TorqueInputModule.java
@@ -0,0 +1,33 @@
+package org.texastorque.torquelib.base;
+
+/**
+ * @apiNote This is a big bruh moment...
+ *          You overload the update method
+ *          You run it using the run method
+ *          (this is for the assist sequence)
+ *
+ * @author Justus (see me for questions)
+ *
+ * @deprecated No longer used in new project structure.
+ */
+@Deprecated(forRemoval = true)
+public abstract class TorqueInputModule {
+    private boolean blocked = false;
+
+    public final void block() { blocked = true; }
+
+    public final void unblock() { blocked = false; }
+
+    public final boolean isBlocked() { return blocked; }
+
+    public abstract void update();
+
+    protected void reset(){};
+
+    public void smartDashboard(){};
+
+    public final void run() {
+        if (!blocked) update();
+        unblock();
+    }
+}
