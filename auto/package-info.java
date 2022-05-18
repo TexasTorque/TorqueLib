b29fee8b04e1c828cd6aa728e36d2e076862d1a6
Juicestus <jus@gtsbr.org>
2022-05-17 21:25:58 -0500

    auto docs + package info

---
+++ auto/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Texas Torque's custom autonomous framework. A collections of
+ * abstract classes and boilerplate code used when programming 
+ * autonomous routines.
+ * 
+ * Created for use in the 2022 Rapid React season. Included in
+ * the 2023 TorqueLib and project structure refactor.
+ * 
+ * The framework provides the following classes:
+ * - TorqueAutoManager
+ * - TorqueBlock
+ * - TorqueCommand
+ * - TorqueEmpty
+ * - TorqueSequence
+ * 
+ * @author Justus Languell
+ */
+package org.texastorque.torquelib.auto;
---
+++ auto/readme.md
@@ -0,0 +1,16 @@
+# Texas Torque Autonomous Framework
+ 
+Texas Torque's custom autonomous framework. A collections of
+abstract classes and boilerplate code used when programming
+autonomous routines.
+
+Created for use in the 2022 Rapid React season. Included in
+the 2023 TorqueLib and project structure refactor.
+
+The framework provides the following classes:
+
+- TorqueAutoManager
+- TorqueBlock
+- TorqueCommand
+- TorqueEmpty
+- TorqueSequence
