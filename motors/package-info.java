d419abc18012bc7b940ff1fa9b86a6ce4d516127
Juicestus <jus@gtsbr.org>
2022-06-02 22:23:47 -0500

    motor package info

---
+++ motors/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains wrapper classes that are used to abstract
+ * common functionality for a motor. The use of common base classes
+ * and interfaces helps with writing generic motor implementations
+ * that can be easily modified if mechanical so choose. :)
+ *
+ * @author Justus Languell
+ * @author Jack Pittenger
+ */
+package org.texastorque.torquelib.motors;
