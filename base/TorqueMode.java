d560faa834ffbfcdfd43a2900117fca552dd0f7d
Juicestus <jus@gtsbr.org>
2022-06-07 01:40:38 -0500

    new subsystem base system

---
+++ base/TorqueMode.java
@@ -0,0 +1,26 @@
+package org.texastorque.torquelib.base;
+
+/**
+ * An enum to describe the state of the robot.
+ * 
+ * @author Justus Languell
+ */
+public enum TorqueMode {
+    DISABLED, AUTO, TELEOP, TEST;
+
+    public final boolean isCompetition() {
+        return this == AUTO || this == TELEOP;
+    }
+
+    public final boolean isDisabled() {
+        return this == DISABLED;
+    }
+
+    public final boolean isTeleop() {
+        return this == TELEOP;
+    }
+
+    public final boolean isAuto() {
+        return this == AUTO;
+    }
+}
--- base/TorqueSubsystem.java
+++ base/TorqueSubsystem.java
@@ -4,18 +4,32 @@ package org.texastorque.torquelib.base;
  * Subsystem template.
  * Extend this class for each subsystem class and override the methods.
  *
- * @author Justus
+ * @author Justus Languell
  */
 public abstract class TorqueSubsystem {
+    @Deprecated
+    public void initTeleop() {}
+    //public abstract void initTeleop();
+    @Deprecated
+    public void updateTeleop() {}
+    //public abstract void updateTeleop();
 
-    public abstract void initTeleop();
-    public abstract void updateTeleop();
+    @Deprecated
+    public void initAuto() {}
+    //public abstract void initAuto();
+    @Deprecated
+    public void updateAuto() {}
+    //public abstract void updateAuto();
 
-    public abstract void initAuto();
-    public abstract void updateAuto();
-
+    @Deprecated
     public void initDisabled() {}
+    @Deprecated
     public void updateDisabled() {}
 
-    public void smartDashboard() {}
+
+    public abstract void initialize(final TorqueMode mode);
+
+    public abstract void update(final TorqueMode mode);
+
+    public void log() {}
 }
