b92a51c549997c21b1aa984753404ea57e9e8f62
Juicestus <jus@gtsbr.org>
2022-06-23 13:16:19 -0500

    generic command builder

--- auto/commands/Generic.java
+++ auto/commands/Generic.java
@@ -8,23 +8,51 @@ public final class Generic extends TorqueCommand  {
     private final Runnable onInit, onContinuous, onEnd;
     private final Supplier<Boolean> condition;
 
-    public Generic(final Runnable onInit, final Runnable onContinuous, 
-            final Supplier<Boolean> condition, final Runnable onEnd) {
-        this.onInit = onInit;
-        this.onContinuous = onContinuous;
-        this.condition = condition;
-        this.onEnd = onEnd;
+    public Generic(final Builder builder) {
+        this.onInit = builder.init;
+        this.onContinuous = builder.continuous;
+        this.onEnd = builder.end;
+        this.condition = builder.condition;
     }
 
     @Override
-    protected final void init() { onInit.run(); }
+    protected final void init() { if (onInit != null) onInit.run(); }
 
     @Override
-    protected final void continuous() { onContinuous.run(); }
+    protected final void continuous() { if (onContinuous != null) onContinuous.run(); }
 
     @Override
-    protected final boolean endCondition() { return condition.get(); }
+    protected final boolean endCondition() { return condition == null ? true : condition.get(); }
 
     @Override
-    protected final void end() { onEnd.run(); }
+    protected final void end() { if (onEnd != null) onEnd.run(); }
+
+    public static final Builder useBuilder() { return new Builder(); }
+
+    public static final class Builder {
+        private Runnable init, continuous, end;
+        private Supplier<Boolean> condition;
+
+        public Builder() {}
+
+        public final Builder onInit(final Runnable init) {
+            this.init = init; return this;
+        }
+
+        public final Builder onContinuous(final Runnable continuous) {
+            this.continuous = continuous; return this;
+        }
+
+        public final Builder onEnd(final Runnable end) {
+            this.end = end; return this;
+        }
+
+        public final Builder addCondition(final Supplier<Boolean> condition) {
+            this.condition = condition; return this;
+        }
+
+        public final Generic build() { return new Generic(this); }
+    }
+
+   
 }
---
+++ auto/test/Test.java
@@ -0,0 +1,14 @@
+package org.texastorque.torquelib.auto.test;
+
+import org.texastorque.torquelib.auto.commands.Generic;
+
+public final class Test {
+    public static final void main(final String[] arguments) {
+        Generic command = Generic.useBuilder()
+                            .onInit(() -> { System.out.println("INIT"); })
+                            .onContinuous(() -> { System.out.println("CONT"); })
+                            .onEnd(() -> { System.out.println("END"); })
+                            .addCondition(() -> { return true; })
+                            .build();
+    }
+}
