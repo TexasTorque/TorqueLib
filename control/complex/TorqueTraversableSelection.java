a57aa804bdb84726c28b85189f329208a5da6536
Juicestus <jus@gtsbr.org>
2022-06-26 15:47:59 -0500

    + TorqueTraversableSelection

---
+++ control/complex/TorqueTraversableSelection.java
@@ -0,0 +1,41 @@
+package org.texastorque.torquelib.control.complex;
+
+import org.texastorque.torquelib.control.TorqueClick;
+
+/**
+ * Wrapper for an array that has a selector that can be 
+ * incremented or decremented on controller inputs.
+ * 
+ * [1, 2, 3, 4, 5]
+ *  <- ^ -> 
+ * 
+ * @author Justus Languell
+ */
+public final class TorqueTraversableSelection<T> {
+    private final TorqueClick increment = new TorqueClick();
+    private final TorqueClick decrement = new TorqueClick();
+
+    private int index, lastIndex;
+    private final T[] values;
+    
+    public TorqueTraversableSelection(final T[] values) {
+        this.values = values;
+    }
+
+    public final T calculate(final boolean decrement, final boolean increment) {
+        if (this.decrement.calculate(decrement)) index = Math.max(index - 1, 0);
+        if (this.increment.calculate(increment)) index = Math.min(index + 1, values.length - 1);
+        return values[index];
+    }
+
+    /**
+     * Check if the class has been updated since the last call of this method.
+     * 
+     * @return Has the class been updated since the last call of this method.
+     */
+    public final boolean hasUpdated() {
+        if (index == lastIndex) return false;
+        return (lastIndex = index) == index;
+    }
+
+}
