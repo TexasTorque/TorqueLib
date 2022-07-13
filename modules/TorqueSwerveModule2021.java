d63f622938bdc295a9f3ab87e62ff8af7d852148
Juicestus <jus@gtsbr.org>
2022-07-13 16:45:23 -0500

    TorqueSwerveModule can use TorquePID

--- modules/TorqueSwerveModule2021.java
+++ modules/TorqueSwerveModule2021.java
@@ -14,6 +14,8 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.texastorque.torquelib.control.TorquePID;
 import org.texastorque.torquelib.modules.base.TorqueSwerveModule;
 import org.texastorque.torquelib.motors.TorqueSparkMax;
 import org.texastorque.torquelib.motors.TorqueTalon;
@@ -42,7 +44,7 @@ public final class TorqueSwerveModule2021 extends TorqueSwerveModule {
 
     private boolean logging = false;
 
-    /**
+     /**
      * Construct a new TorqueSwerveModule2021.
      *
      * @param id                The id of the swerve module.
@@ -56,6 +58,54 @@ public final class TorqueSwerveModule2021 extends TorqueSwerveModule {
      * @param maxAcceleration   The maximum acceleration of the drive motor in meters per second per second.
      * @param driveFeedForward  The drive motor feed forward for autonomous.
      */
+    public TorqueSwerveModule2021(final int id, final int drivePort, final int rotatePort, final double driveGearing,
+                                  final double wheelRadiusMeters, final TorquePID drivePID, final TorquePID rotatePID,
+                                  final double maxVelocity, final double maxAcceleration,
+                                  final SimpleMotorFeedforward driveFeedForward) {
+        super(id);
+        // invCoef = ((id & 1) << 1) - 1; // WTF? -> invCoef = id % 2 == 0 ? -1 : 1;
+        invCoef = 1;
+
+        drive = new TorqueSparkMax(drivePort);
+        drive.configurePID(drivePID);
+        // drive.configureSmartMotion(
+        //         metersPerSecondToEncoderPerMinute(this.maxVelocity = maxVelocity),
+        //         metersPerSecondToEncoderPerMinute(.1),
+        //         metersPerSecondToEncoderPerMinute(maxAcceleration),
+        //         metersPerSecondToEncoderPerMinute(.1),
+        //         0);
+
+        drive.setSupplyLimit(40);
+        drive.burnFlash();
+
+        rotate = new TorqueTalon(rotatePort);
+        rotate.setSupplyLimit(new SupplyCurrentLimitConfiguration(true, 5, 10, .03));
+        rotate.configurePID(rotatePID);
+        rotate.zeroEncoder();
+
+        this.driveGearing = driveGearing;
+        this.wheelRadiusMeters = wheelRadiusMeters;
+        this.driveFeedForward = driveFeedForward;
+        this.maxVelocity = maxVelocity;
+    }
+
+    /**
+     * Construct a new TorqueSwerveModule2021.
+     *
+     * @param id                The id of the swerve module.
+     * @param drivePort         The port (can id) of the drive motor.
+     * @param rotatePort        The port (can id) of the rotation motor.
+     * @param driveGearing      The drive motor gearing.
+     * @param wheelRadiusMeters The radius of the wheel in meters.
+     * @param drivePID          The drive motor PID.
+     * @param rotatePID         The rotation motor PID.
+     * @param maxVelocity       The maximum velocity of the drive motor in meters per second.
+     * @param maxAcceleration   The maximum acceleration of the drive motor in meters per second per second.
+     * @param driveFeedForward  The drive motor feed forward for autonomous.
+     * 
+     * @deprecated Uses KPID, use TorquePID instead.
+     */
+    @Deprecated 
     public TorqueSwerveModule2021(final int id, final int drivePort, final int rotatePort, final double driveGearing,
                                   final double wheelRadiusMeters, final KPID drivePID, final KPID rotatePID,
                                   final double maxVelocity, final double maxAcceleration,
