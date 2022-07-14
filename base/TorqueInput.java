7f389c533ad51ca077ca52ee37ea61acae258a8f
Juicestus <jus@gtsbr.org>
2022-07-14 14:49:09 -0500

    Generic input baseclass

--- base/TorqueInput.java
+++ base/TorqueInput.java
@@ -6,21 +6,13 @@
  */
 package org.texastorque.torquelib.base;
 
-import org.texastorque.torquelib.util.GenericController;
-
 /**
  * Input base class. Holds references to controllers.
- *
+ * 
  * @author Justus Languell
- * 
- * @deprecated Holds references to {@link GenericController}, which is deprecated.
- * 
- * @apiNote Use {@link TorqueInput2} until this class is updated.
  */
-public abstract class TorqueInput {
-    protected GenericController driver, operator;
+public abstract class TorqueInput<T> {
+    protected T driver, operator;
 
     public abstract void update();
-
-    public void smartDashboard() {}
 }
