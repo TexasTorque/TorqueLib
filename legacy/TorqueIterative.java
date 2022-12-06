1e38b1a24a2c27258e2b760659d0502f3dd14fb6
Juicestus <jus@gtsbr.org>
2022-12-06 10:26:22 -0600

    Moved TorqueInterative and deleted SubsystemState

--- legacy/TorqueIterative.java
+++ legacy/TorqueIterative.java
@@ -4,7 +4,7 @@
  * This file is part of TorqueLib, which is licensed under the MIT license.
  * For more details, see ./license.txt or write <jus@justusl.com>.
  */
-package org.texastorque.torquelib.base;
+package org.texastorque.torquelib.legacy;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
