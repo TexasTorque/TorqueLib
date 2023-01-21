3da8f78a9e741641dc97dca3f4f0cba6975e5ef7
0marA <theengineermachine@gmail.com>
2023-01-21 13:57:31 -0600

    TorqueCurrentSpike

---
+++ control/TorqueCurrentSpike.java
@@ -0,0 +1,38 @@
+package org.texastorque.torquelib.control;
+
+public final class TorqueCurrentSpike {
+
+    private CurrentSpikes status = CurrentSpikes.NONE;
+    private final TorqueClick currentClick = new TorqueClick();
+
+    public static enum CurrentSpikes {
+        NONE, STARTUP, SPIKE;
+
+        public CurrentSpikes next() {
+            return values()[Math.min(ordinal() + 1, values().length - 1)];
+        }
+
+    }
+
+    private final double currentLimit;
+
+    public TorqueCurrentSpike(final double currentLimit) {
+        this.currentLimit = currentLimit;
+    }
+
+    public boolean calculate(final double currentCurrent) {
+        if (currentClick.calculate(currentCurrent >= currentLimit)) 
+            status = status.next();
+
+        return status == CurrentSpikes.SPIKE;
+    }
+
+    public void reset() {
+        status = CurrentSpikes.NONE;
+    }
+
+    public CurrentSpikes getStatus() {
+        return status;
+    }
+
+}
