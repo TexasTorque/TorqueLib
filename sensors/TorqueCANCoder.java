6fd59c7b0b36af6ecebd9a9325f4c70c3d8a1717
Juicestus <jus@gtsbr.org>
2023-02-16 23:27:42 -0600

    Fixed TorqueCANCoder::isCANResponsive

--- sensors/TorqueCANCoder.java
+++ sensors/TorqueCANCoder.java
@@ -2,6 +2,11 @@ package org.texastorque.torquelib.sensors;
 
 import com.ctre.phoenix.sensors.CANCoder;
 
+/**
+ * A CANCoder that can be used to check if it is CAN responsive.
+ * 
+ * @author Omar Afzal
+ */
 public final class TorqueCANCoder extends CANCoder {
     private double lastTimestamp = 0;
 
@@ -17,7 +22,6 @@ public final class TorqueCANCoder extends CANCoder {
     }
 
     public final boolean isCANResponsive() {
-        return getDeltaTimestamp() == 0;
+        return getDeltaTimestamp() != 0;
     }
-
 }
