ff201821df08c965fab5191327239c27492e49c7
0marA <theengineermachine@gmail.com>
2023-10-03 18:12:46 -0500

    Add driveFF to 2022 swerve

--- swerve/TorqueSwerveModule2022.java
+++ swerve/TorqueSwerveModule2022.java
@@ -1,19 +1,17 @@
 /**
  * Copyright 2011-2023 Texas Torque.
  *
- * This file is part of TorqueLib, which is licensed under the MIT license.
- * For more details, see ./license.txt or write <jus@justusl.com>.
+ * This file is part of TorqueLib, which is licensed under the MIT license. For more details, see
+ * ./license.txt or write <jus@justusl.com>.
  */
 package org.texastorque.torquelib.swerve;
 
 import org.texastorque.torquelib.motors.TorqueNEO;
 import org.texastorque.torquelib.swerve.base.TorqueSwerveModule;
-
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.ctre.phoenix.sensors.SensorTimeBase;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -28,20 +26,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * https://drive.google.com/file/d/1bvgo75gqLzoerBIEtEObXep_6gpac6mZ/view?usp=sharing
  * https://drive.google.com/file/d/1q4cmx3Ntscfynn_1Tc8W9VAqR9Tgn0NT/view?usp=sharing
  *
- * The module uses a Rev NEO for drive and rotation control. It uses a CTRE Cancoder
- * positioned 1:1 with the wheel rotation.
+ * The module uses a Rev NEO for drive and rotation control. It uses a CTRE Cancoder positioned 1:1
+ * with the wheel rotation.
  *
  * The code is kinda based off the FRC 1706 swerve module.
  *
- * Cancoder docs: https://api.ctr-electronics.com/phoenix/release/java/com/ctre/phoenix/sensors/CANCoder.html
+ * Cancoder docs:
+ * https://api.ctr-electronics.com/phoenix/release/java/com/ctre/phoenix/sensors/CANCoder.html
  *
  * The rotation of the modules is [0, 2π) radians, with 0 being straight ahead (I think).
  *
- *     0 or 2π
- *        ↑
- *  π/2 ← * → 3π/2
- *        ↓
- *        π
+ * 0 or 2π ↑ π/2 ← * → 3π/2 ↓ π
  *
  * @author Justus Languell
  */
@@ -50,33 +45,35 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
     /**
      * A structure to define the constants for the swerve module.
      *
-     * Has default values that can be overriden before written to
-     * the module.
+     * Has default values that can be overriden before written to the module.
      */
     public static final class SwerveConfig {
         public static final SwerveConfig defaultConfig = new SwerveConfig();
 
         public double magic = 6.57 / (8.0 + 1.0 / 3.0);
 
-        public int driveMaxCurrent = 35,          // amps
-                turnMaxCurrent = 25;              // amps
+        public int driveMaxCurrent = 35, // amps
+                turnMaxCurrent = 25; // amps
         public double voltageCompensation = 12.6, // volts
-                maxVelocity = 3.25,               // m/s
-                maxAcceleration = 3.0,            // m/s^2
-                maxAngularVelocity = Math.PI,     // radians/s
+                maxVelocity = 3.25, // m/s
+                maxAcceleration = 3.0, // m/s^2
+                maxAngularVelocity = Math.PI, // radians/s
                 maxAngularAcceleration = Math.PI, // radians/s
 
                 // The following will most likely need to be overriden
                 // depending on the weight of each robot
-                driveStaticGain = 0.015, driveFeedForward = 0.212, drivePGain = 0.2, driveIGain = 0.0, driveDGain = 0.0,
+                driveStaticGain = 0.015, driveFeedForward = 0.212, drivePGain = 0.2,
+                driveIGain = 0.0, driveDGain = 0.0,
 
-                      driveRampRate = 3.0,    // %power/s
-                driveGearRatio = 6.57,        // Translation motor to wheel
+                driveRampRate = 3.0, // %power/s
+                driveGearRatio = 6.57, // Translation motor to wheel
                 wheelDiameter = 4.0 * 0.0254, // m
                 driveVelocityFactor = (1.0 / driveGearRatio / 60.0) * (wheelDiameter * Math.PI), // m/s
-                drivePoseFactor = (1.0 / driveGearRatio) * (wheelDiameter * Math.PI),            // m
-                turnPGain = 0.5, turnIGain = 0.0, turnDGain = 0.0,
-                      turnGearRatio = 12.41; // Rotation motor to wheel
+                drivePoseFactor = (1.0 / driveGearRatio) * (wheelDiameter * Math.PI), // m
+                turnPGain = 0.3, turnIGain = 0.0, turnDGain = 0.0, turnGearRatio = 12.41; // Rotation
+                                                                                          // motor
+                                                                                          // to
+                                                                                          // wheel
     }
 
     public static final class SwervePorts {
@@ -98,17 +95,21 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
     public static void normalize(SwerveModuleState[] states, final double max) {
         double top = 0, buff;
         for (final SwerveModuleState state : states)
-            if ((buff = (state.speedMetersPerSecond / max)) > top) top = buff;
+            if ((buff = (state.speedMetersPerSecond / max)) > top)
+                top = buff;
         if (top != 0)
-            for (SwerveModuleState state : states) state.speedMetersPerSecond /= top;
+            for (SwerveModuleState state : states)
+                state.speedMetersPerSecond /= top;
     }
 
     private static double coterminal(final double rotation) {
         double coterminal = rotation;
         final double full = Math.signum(rotation) * 2 * Math.PI;
-        while (coterminal > Math.PI || coterminal < -Math.PI) coterminal -= full;
+        while (coterminal > Math.PI || coterminal < -Math.PI)
+            coterminal -= full;
         return coterminal;
     }
+
     private final SwerveConfig config;
 
     // The NEO motors for turn and drive.
@@ -130,13 +131,14 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
 
     public boolean useCancoder = true;
 
-    public TorqueSwerveModule2022(final String name, final SwervePorts ports, final double staticOffset,
-                                  final SwerveConfig config) {
-        this(name, ports.drive, ports.turn, ports.encoder, staticOffset, config);
+    public TorqueSwerveModule2022(final String name, final SwervePorts ports,
+            final double staticOffset, final SwerveConfig config, final double driveFF) {
+        this(name, ports.drive, ports.turn, ports.encoder, staticOffset, config, driveFF);
     }
 
-    public TorqueSwerveModule2022(final String name, final int driveID, final int turnID, final int encoderID,
-                                  final double staticOffset, final SwerveConfig config) {
+    public TorqueSwerveModule2022(final String name, final int driveID, final int turnID,
+            final int encoderID, final double staticOffset, final SwerveConfig config,
+            final double driveFF) {
         super(driveID);
         this.name = name.replaceAll(" ", "_").toLowerCase();
         this.staticOffset = staticOffset;
@@ -172,7 +174,7 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
         drivePID = new PIDController(config.drivePGain, config.driveIGain, config.driveDGain);
         turnPID = new PIDController(config.turnPGain, config.turnIGain, config.turnDGain);
         turnPID.enableContinuousInput(-Math.PI, Math.PI);
-        driveFeedForward = new SimpleMotorFeedforward(config.driveStaticGain, config.driveFeedForward);
+        driveFeedForward = new SimpleMotorFeedforward(config.driveStaticGain, driveFF);
     }
 
     @Override
@@ -185,7 +187,8 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
 
         // Calculate drive output
         if (useSmartDrive) {
-            final double drivePIDOutput = drivePID.calculate(drive.getVelocity(), optimized.speedMetersPerSecond);
+            final double drivePIDOutput =
+                    drivePID.calculate(drive.getVelocity(), optimized.speedMetersPerSecond);
             final double driveFFOutput = driveFeedForward.calculate(optimized.speedMetersPerSecond);
             // log("Drive PID Output", drivePIDOutput + driveFFOutput);
             drive.setPercent(drivePIDOutput + driveFFOutput);
@@ -194,7 +197,8 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
         }
 
         // Calculate turn output
-        final double turnPIDOutput = turnPID.calculate(getTurnEncoder(), optimized.angle.getRadians());
+        final double turnPIDOutput =
+                turnPID.calculate(getTurnEncoder(), optimized.angle.getRadians());
         // log("Turn PID Output", turnPIDOutput);
         turn.setPercent(turnPIDOutput);
     }
@@ -204,7 +208,9 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
         return new SwerveModuleState(drive.getVelocity(), getRotation());
     }
 
-    public SwerveModulePosition getPosition() { return new SwerveModulePosition(drive.getPosition(), getRotation()); }
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(drive.getPosition(), getRotation());
+    }
 
     @Override
     public Rotation2d getRotation() {
@@ -216,14 +222,20 @@ public final class TorqueSwerveModule2022 extends TorqueSwerveModule {
         turn.setPercent(0.0);
     }
 
-    public void zero() { turn.setPercent(log("zero pid", turnPID.calculate(getTurnEncoder(), 0))); }
+    public void zero() {
+        turn.setPercent(log("zero pid", turnPID.calculate(getTurnEncoder(), 0)));
+    }
 
-    private double getTurnEncoder() { return useCancoder ? getTurnCancoder() : getTurnNEOEncoder(); }
+    private double getTurnEncoder() {
+        return useCancoder ? getTurnCancoder() : getTurnNEOEncoder();
+    }
 
-    private double getTurnNEOEncoder() { return coterminal(turn.getPosition()); }
+    private double getTurnNEOEncoder() {
+        return coterminal(turn.getPosition());
+    }
 
     // public static double coterminal(final double rotation) {
-    //     return rotation % (2 * Math.PI);
+    // return rotation % (2 * Math.PI);
     // }
 
     private double getTurnCancoder() {
