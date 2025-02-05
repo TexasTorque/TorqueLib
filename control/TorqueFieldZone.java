2b725123fb111bd9523522727624d891ca5677dc
maazie2000 <fowlartemis34@gmail.com>
2025-02-05 17:36:14 -0600

    feat: field zone is polygon

--- control/TorqueFieldZone.java
+++ control/TorqueFieldZone.java
@@ -4,18 +4,36 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class TorqueFieldZone {
-
-	private final Translation2d start, end;
-
-	public TorqueFieldZone(Translation2d start, Translation2d end) {
-		this.start = start;
-		this.end = end;
+	Translation2d[] points;
+	public TorqueFieldZone(Translation2d ...points) {
+		this.points = points;
 	}
 
 	// Returns whether pose is contained between start (inclusive) and end (exclusive)
+	// Utilizes ray-casting algorithm
 	public boolean contains(final Pose2d pose) {
-		final boolean x = pose.getX() >= start.getX() && pose.getX() < end.getX();
-		final boolean y = pose.getY() >= start.getY() && pose.getY() < end.getY();
-		return x && y;
+		double x = pose.getX();
+		double y = pose.getY();
+		int count = 0;
+
+		for(int i=0; i<points.length; i++){
+			if(x == points[i].getX() || y == points[i].getY()){
+				return true;
+			}
+		}
+
+		for (int i=0; i<points.length; i++){
+			Translation2d point1 = points[i];
+			Translation2d point2 = points[(i+1)%points.length];
+
+			if((y > Math.min(point1.getY(), point2.getY())) && (y <= Math.max(point1.getY(), point2.getY())) && (x < Math.max(point1.getX(), point2.getX()))){
+				double xIntersect = (y-point1.getY())*(point2.getX()-point1.getX())/(point2.getY()-point1.getY())+point1.getX();
+				if(point1.getX() == point2.getX() || x <= xIntersect){
+					count++;
+				}
+			}
+		}
+
+		return count % 2 == 1;
 	}
 }
