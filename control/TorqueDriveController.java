53bbcb4824da08eae98f077c036fa73d8db9848a
humandavey <davey.adams.three@gmail.com>
2025-03-24 20:33:23 -0500

    update drivecontroller

--- control/TorqueDriveController.java
+++ control/TorqueDriveController.java
@@ -9,12 +9,12 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class TorqueDriveController {
 
 	private final ProfiledPIDController xController, yController, thetaController;
 	private Pose2d lastTargetPose;
-	private Pose2d lastCurrentPose;
 
 	public TorqueDriveController(final PIDConstants translationConstants, final TrapezoidProfile.Constraints translationConstraints, final PIDConstants rotationConstants, final TrapezoidProfile.Constraints rotationConstraints) {
 		this.xController = new ProfiledPIDController(translationConstants.kP, translationConstants.kI, translationConstants.kD, translationConstraints);
@@ -25,7 +25,6 @@ public class TorqueDriveController {
 	}
 
 	public TorqueSwerveSpeeds calculate(final Pose2d currentPose, final Pose2d targetPose) {
-		lastCurrentPose = currentPose;
 		if (!targetPose.equals(lastTargetPose)) {
 			reset();
 		}
@@ -41,10 +40,20 @@ public class TorqueDriveController {
 		return speeds;
 	}
 
-	private void reset() {
-		ChassisSpeeds speeds = TorqueSwerveSpeeds.fromRobotRelativeSpeeds(Drivebase.getInstance().lastRealInputSpeeds, lastCurrentPose.getRotation());
-		xController.reset(lastCurrentPose.getX(), speeds.vxMetersPerSecond);
-		yController.reset(lastCurrentPose.getY(), speeds.vyMetersPerSecond);
-		thetaController.reset(lastCurrentPose.getRotation().getRadians(), speeds.omegaRadiansPerSecond);
+	public void reset() {
+		Pose2d currentPose = Drivebase.getInstance().getPose();
+		ChassisSpeeds speeds = null;
+
+		System.out.println("RESET");
+		
+		if (DriverStation.isAutonomous()) {
+			speeds = TorqueSwerveSpeeds.fromRobotRelativeSpeeds(Drivebase.getInstance().lastRealInputSpeeds, currentPose.getRotation());
+		} else {
+			speeds = TorqueSwerveSpeeds.fromRobotRelativeSpeeds(Drivebase.getInstance().inputSpeeds, currentPose.getRotation());
+		}
+		System.out.println(speeds);
+		xController.reset(currentPose.getX(), speeds.vxMetersPerSecond);
+		yController.reset(currentPose.getY(), speeds.vyMetersPerSecond);
+		thetaController.reset(currentPose.getRotation().getRadians(), speeds.omegaRadiansPerSecond);
 	}
 }
