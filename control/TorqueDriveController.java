617ede40a89c471085ccd0ee68af30dc20df2eec
humandavey <davey.adams.three@gmail.com>
2025-03-25 18:22:39 -0500

    fix: drive controller

--- control/TorqueDriveController.java
+++ control/TorqueDriveController.java
@@ -1,55 +1,45 @@
 package org.texastorque.torquelib.control;
 
+import org.littletonrobotics.junction.Logger;
 import org.texastorque.subsystems.Drivebase;
+import org.texastorque.torquelib.Debug;
 import org.texastorque.torquelib.swerve.TorqueSwerveSpeeds;
 
 import com.pathplanner.lib.config.PIDConstants;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DriverStation;
 
 public class TorqueDriveController {
 
 	private final ProfiledPIDController xController, yController, thetaController;
-	private Pose2d lastTargetPose;
 
 	public TorqueDriveController(final PIDConstants translationConstants, final TrapezoidProfile.Constraints translationConstraints, final PIDConstants rotationConstants, final TrapezoidProfile.Constraints rotationConstraints) {
 		this.xController = new ProfiledPIDController(translationConstants.kP, translationConstants.kI, translationConstants.kD, translationConstraints);
 		this.yController = new ProfiledPIDController(translationConstants.kP, translationConstants.kI, translationConstants.kD, translationConstraints);
 		this.thetaController = new ProfiledPIDController(rotationConstants.kP, rotationConstants.kI, rotationConstants.kD, rotationConstraints);
 		this.thetaController.enableContinuousInput(-Math.PI, Math.PI);
-		this.lastTargetPose = new Pose2d();
 	}
 
 	public TorqueSwerveSpeeds calculate(final Pose2d currentPose, final Pose2d targetPose) {
+		Debug.log("Align Target Pose", targetPose.toString());
+		Logger.recordOutput("Align Target Pose", targetPose);
+
 		double xPower = xController.calculate(currentPose.getX(), targetPose.getX());
 		double yPower = yController.calculate(currentPose.getY(), targetPose.getY());
 		double thetaPower = thetaController.calculate(currentPose.getRotation().getRadians(), targetPose.getRotation().getRadians());
 
 		TorqueSwerveSpeeds speeds = new TorqueSwerveSpeeds(xPower, yPower, thetaPower);
 
-		lastTargetPose = targetPose;
-
 		return speeds;
 	}
 
 	public void reset() {
 		Pose2d currentPose = Drivebase.getInstance().getPose();
-		ChassisSpeeds speeds = null;
 
-		System.out.println("RESET");
-		
-		if (DriverStation.isAutonomous()) {
-			speeds = TorqueSwerveSpeeds.fromRobotRelativeSpeeds(Drivebase.getInstance().lastRealInputSpeeds, currentPose.getRotation());
-		} else {
-			speeds = TorqueSwerveSpeeds.fromRobotRelativeSpeeds(Drivebase.getInstance().inputSpeeds, currentPose.getRotation());
-		}
-		System.out.println(speeds);
-		xController.reset(currentPose.getX(), speeds.vxMetersPerSecond);
-		yController.reset(currentPose.getY(), speeds.vyMetersPerSecond);
-		thetaController.reset(currentPose.getRotation().getRadians(), speeds.omegaRadiansPerSecond);
+		xController.reset(currentPose.getX(), 0);
+		yController.reset(currentPose.getY(), 0);
+		thetaController.reset(currentPose.getRotation().getRadians(), 0);
 	}
 }
