85cf565b5bbd3a558cb33c01c134b56835b7a086
humandavey <davey.adams.three@gmail.com>
2025-03-29 00:00:25 -0500

    fix: refactor

--- control/TorqueDriveController.java
+++ control/TorqueDriveController.java
@@ -2,46 +2,34 @@ package org.texastorque.torquelib.control;
 
 import org.littletonrobotics.junction.Logger;
 import org.texastorque.Subsystems;
-import org.texastorque.subsystems.Drivebase;
 import org.texastorque.torquelib.Debug;
 import org.texastorque.torquelib.swerve.TorqueSwerveSpeeds;
-
 import com.pathplanner.lib.config.PIDConstants;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.Timer;
 
 public class TorqueDriveController implements Subsystems {
 
 	private final PIDController xController, yController, thetaController;
-	private final Timer timer;
 	
 	public TorqueDriveController(final PIDConstants translationConstants, final PIDConstants rotationConstants) {
 		this.xController = new PIDController(translationConstants.kP, translationConstants.kI, translationConstants.kD);
 		this.yController = new PIDController(translationConstants.kP, translationConstants.kI, translationConstants.kD);
 		this.thetaController = new PIDController(rotationConstants.kP, rotationConstants.kI, rotationConstants.kD);
 		this.thetaController.enableContinuousInput(-Math.PI, Math.PI);
-		this.timer = new Timer();
 	}
 	
 	public TorqueSwerveSpeeds calculate(final Pose2d currentPose, final Pose2d targetPose) {
-
-		Pair<Double, Double> offsets = getOffsets(targetPose, currentPose);
-		double forward = offsets.getFirst();
-		double right = offsets.getSecond();
-		double desiredForward = forward;
-		double desiredRight = right;
-		double slope = .3;
-		Pose2d desiredPose;
+		final Pair<Double, Double> offsets = getOffsets(targetPose, currentPose);
+		double desiredForward = offsets.getFirst();
+		double desiredRight = offsets.getSecond();
+		final double SLOPE = .3;
 	
-		desiredForward *= slope;
+		desiredForward *= SLOPE;
 
-		desiredPose = new Pose2d(
+		Pose2d desiredPose = new Pose2d(
 			currentPose.getX() + (desiredForward * Math.cos(targetPose.getRotation().getRadians())) + (desiredRight * Math.cos(targetPose.getRotation().getRadians() + Math.PI / 2)),
 			currentPose.getY() + (desiredForward * Math.sin(targetPose.getRotation().getRadians())) + (desiredRight * Math.sin(targetPose.getRotation().getRadians() + Math.PI / 2)),
 			targetPose.getRotation()
@@ -57,15 +45,6 @@ public class TorqueDriveController implements Subsystems {
 		Logger.recordOutput("Align Target Pose", desiredPose);
 
 		return speeds;
-	}
-
-	public void reset() {
-		// Pose2d currentPose = Drivebase.getInstance().getPose();
-		// ChassisSpeeds currentSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(drivebase.inputSpeeds, currentPose.getRotation());
-
-		// xController.reset(currentPose.getX(), currentSpeeds.vxMetersPerSecond);
-		// yController.reset(currentPose.getY(), currentSpeeds.vyMetersPerSecond);
-		// thetaController.reset(currentPose.getRotation().getRadians(), currentSpeeds.omegaRadiansPerSecond);
 	}
 
 	public Pair<Double, Double> getOffsets(final Pose2d targetPose, final Pose2d currentPose) {
