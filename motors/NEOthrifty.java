ccc01cebb06a466cbaa6f5b225774ba53c671392
James H <heffnjam@gmail.com>
2026-02-04 18:03:49 -0600

    feat: thrifty motor controller

---
+++ motors/NEOthrifty.java
@@ -0,0 +1,105 @@
+package org.texastorque.torquelib.motors;
+
+import org.texastorque.torquelib.control.TorqueDebug;
+import com.thethriftybot.devices.ThriftyNova;
+import com.thethriftybot.devices.ThriftyNova.ThriftyNovaConfig;
+import com.thethriftybot.interfaces.*;
+import com.thethriftybot.server.*;
+import com.thethriftybot.util.*;
+import com.thethriftybot.wrappers.*;
+
+/**
+ * Wrapper for the 2025 RevLib rewrite of tihe SparkMax
+ * class and configuration library
+ * 
+ * @author Davey Adams
+ */
+public class NEOthrifty {
+	
+	private final ThriftyNova motor;
+	private final ThriftyNovaConfig config;
+
+	public NEOthrifty(final int port) {
+		motor = new ThriftyNova(port);
+		config = new ThriftyNovaConfig(false, true,null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+	}
+
+/*
+	public NEOthrifty idleMode(final IdleMode mode) {
+		config.idleMode(mode);
+
+		return this;
+	}
+
+	public NEOthrifty pid(final double p, final double i, final double d) {
+		config.closedLoop.pid(p, i, d);
+		return this;
+	}
+
+	public NEOthrifty voltageCompensation(final double voltage) {
+		config.voltageCompensation(voltage);
+		return this;
+	}
+
+	public NEOthrifty disableVoltageCompensation() {
+		config.disableVoltageCompensation();
+		return this;
+	}
+
+	public NEOthrifty currentLimit(final int limit) {
+		config.smartCurrentLimit(limit);
+		return this;
+	}
+
+	public NEOthrifty conversionFactors(final double posFactor, final double veloFactor) {
+		config.encoder.positionConversionFactor(posFactor);
+		config.encoder.velocityConversionFactor(veloFactor);
+		return this;
+	}
+
+	public NEOthrifty apply() {
+		motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+		return this;
+	}
+*/
+	// public NEOthrifty debug(final String name) {
+    //     new TorqueDebug(this, name);
+    //     return this;
+    // }
+
+	public void setPercent(final double percent) {
+		motor.set(percent);
+	}
+
+	public double getPercent() {
+		return motor.get();
+	}
+
+	public void setVolts(final double volts) {
+		motor.setVoltage(volts);
+	}
+ 
+	public double getVolts() {
+		return motor.getVoltage();
+	}
+/* 
+	public double getBusVoltage() {
+		return motor.getBusVoltage();
+	}
+
+	public double getOutputCurrent() {
+		return motor.getOutputCurrent();
+	}
+
+	public double getPosition() {
+		return motor.getEncoder().getPosition();
+	}
+
+	public double getVelocity() {
+		return motor.getEncoder().getVelocity();
+	}
+
+	public void setPosition(final double position) {
+		motor.getEncoder().setPosition(position);
+	}*/
+}
